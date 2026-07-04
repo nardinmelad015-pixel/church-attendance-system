74889bb9fadd412b3b319bf4f93cf7d564086241
Nardin Melad <nardinmelad015@gmail.com>
2026-07-04 17:10:09 +0300

    Add STATIC_ROOT

--- church_system_project/settings.py
+++ church_system_project/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = []
